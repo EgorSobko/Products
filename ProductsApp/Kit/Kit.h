d783360dd9de776d03e5900c05054622f08f4001
Egor Sobko <egor.sobko88@gmail.com>
2017-10-10 23:13:56 +0300

    added project

---
+++ ProductsApp/Kit/Kit.h
@@ -0,0 +1,19 @@
+//
+//  Kit.h
+//  Kit
+//
+//  Created by Egor Sobko on 10.10.17.
+//  Copyright © 2017 Egor Sobko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kit.
+FOUNDATION_EXPORT double KitVersionNumber;
+
+//! Project version string for Kit.
+FOUNDATION_EXPORT const unsigned char KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kit/PublicHeader.h>
+
+
